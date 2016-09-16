63f7adaad097b751fc0f4b882e10ca4114625a53
andrewbainter13 <andrewbainter13@OLINLAB204-16A.augustana.edu>
2016-09-15 22:36:52 -0500

    added edit dynamics

--- ProjectGUI/HomeScreen.java
+++ ProjectGUI/HomeScreen.java
@@ -15,7 +15,7 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class HomeScreen implements ActionListener {
-	
+
 	private JFrame frame;
 	private JButton search;
 	private JButton add;
@@ -23,7 +23,6 @@ public class HomeScreen implements ActionListener {
 	private JButton artists;
 	private JButton connections;
 	private JPanel bigPanel;
-	
 
 	public HomeScreen() {
 		frame = new JFrame();
@@ -38,22 +37,23 @@ public class HomeScreen implements ActionListener {
 		artists = new JButton("Artists");
 		connections = new JButton("Connections");
 
-		JPanel westPanel = new JPanel(new GridLayout(3,1));
+		JPanel westPanel = new JPanel(new GridLayout(3, 1));
 		westPanel.add(search);
 		westPanel.add(add);
 		westPanel.add(edit);
-		
-		JPanel northPanel = new JPanel(new GridLayout(1,5));
+
+		JPanel northPanel = new JPanel(new GridLayout(1, 5));
 		northPanel.add(artists);
 		northPanel.add(connections);
-		
-		JPanel centerPanel = new JPanel(new BorderLayout()); //curently does nothing
-		
-		bigPanel = new JPanel(new BorderLayout()); 
+
+		JPanel centerPanel = new JPanel(new BorderLayout()); // curently does
+																// nothing
+
+		bigPanel = new JPanel(new BorderLayout());
 		bigPanel.add(northPanel, BorderLayout.NORTH);
 
 		frame.add(westPanel, BorderLayout.WEST);
-		frame.add(bigPanel,BorderLayout.CENTER);
+		frame.add(bigPanel, BorderLayout.CENTER);
 
 		search.addActionListener(this);
 		add.addActionListener(this);
@@ -67,38 +67,45 @@ public class HomeScreen implements ActionListener {
 	public void actionPerformed(ActionEvent event) {
 		if (event.getSource() == search) {
 			Search search = new Search();
-			
+
 			System.out.println("hi");
-			
-			JPanel southPanel = new JPanel(new GridLayout(1,3));
+
+			JPanel southPanel = new JPanel(new GridLayout(1, 3));
 			JButton clear = new JButton("Clear");
 			JButton edit = new JButton("Edit");
 			JButton export = new JButton("Export");
-			
+
 			southPanel.add(clear);
 			southPanel.add(edit);
 			southPanel.add(export);
-			
+
 			bigPanel.add(southPanel, BorderLayout.SOUTH);
 			bigPanel.revalidate();
-			
+
 			System.out.println("bye");
-		} else if(event.getSource() == add){
-			Object[] options = {"Add Artist", "Add Connection"};
-			int val=JOptionPane.showOptionDialog(frame, "What would you like to add?", "Answer me", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
-			if(val == 0){ //if artist
+		} else if (event.getSource() == add) {
+			Object[] options = { "Add Artist", "Add Connection" };
+			int val = JOptionPane.showOptionDialog(frame, "What would you like to add?", "Answer me",
+					JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
+			if (val == 0) { // if artist
 				artists.setPressedIcon(new ImageIcon());
 				AddArtist newArtist = new AddArtist();
-			} else { //if connection
+			} else { // if connection
 				connections.setPressedIcon(new ImageIcon());
 				AddConnection newConnection = new AddConnection();
 			}
-			if(true){
-				//make add connection
-			}else{
-				//make add artist
-				AddArtist artist = new AddArtist();
-			}
+		} else if (event.getSource() == edit) {
+
+			JPanel southPanel = new JPanel(new GridLayout(1, 2));
+			JButton edit = new JButton("Edit");
+			JButton searchEdit = new JButton("Search for Edit");
+
+			southPanel.add(edit);
+			southPanel.add(searchEdit);
+
+			bigPanel.add(southPanel, BorderLayout.SOUTH);
+			bigPanel.revalidate();
+
 		}
 	}
 }
