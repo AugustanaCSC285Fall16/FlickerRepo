1910e23b950125c0a8b8205e43a6613d0d84bcb2
laurenjohnson13 <laurenjohnson13@OLINLAB204-15A.augustana.edu>
2016-10-13 23:27:40 -0500

    Changes:
    Changed where the Add Vocab Button was. 
    Added Add vocab functionality and csvs.

--- FlickerProject/src/AddEditPersonGUI.java
+++ FlickerProject/src/AddEditPersonGUI.java
@@ -13,18 +13,20 @@ import javax.swing.*;
 
 public class AddEditPersonGUI implements ActionListener {
 	
-	private static final int WIDTH = 260;
-	private static final int HEIGHT = 250;
+	private static final int WIDTH = 350;
+	private static final int HEIGHT = 300;
 	private static final String[] FIELDS = new String[]{"", "Cultural ID", "Gender", "Occupation"};
 
 	private JFrame frame;
 	private JTextField name;
-	private JComboBox<String> culture;
+	private JTextField nodeName;
+	private JComboBox<Object> culture;
 	private JComboBox<String> gender;
-	private JComboBox<String> occupation;
+	private JComboBox<Object> occupation;
 	private JTextArea notes;
 	
 	private JPanel namePanel;
+	private JPanel nodeNamePanel;
 	private JPanel culturePanel;
 	private JPanel cultureFlowPanel;
 	private JPanel newCultureFlowPanel;
@@ -36,6 +38,7 @@ public class AddEditPersonGUI implements ActionListener {
 	private JPanel southPanel;
 	
 	private JLabel nameLabel;
+	private JLabel nodeNameLabel;
 	private JLabel culturalLabel;
 	private JLabel genderLabel;
 	private JLabel occupationLabel;
@@ -44,16 +47,16 @@ public class AddEditPersonGUI implements ActionListener {
 	JButton submitButton;
 	JButton deleteButton;
 	private JButton cancel;
-	private JButton addData;
 	HomeScreenGUI home;
 	private JScrollPane scroll;
 	
+	DataStorage storage;
 	private boolean editing;
 
 	// class that relates to controlled vocabulary
-	private Vector<String> cultureChoices;
+	private ArrayList<String> cultureChoices;
 	private Vector<String> genderChoices;
-	private Vector<String> occupationChoices;
+	private ArrayList<String> occupationChoices;
 
 	private Person personEdited;
 
@@ -69,29 +72,37 @@ public class AddEditPersonGUI implements ActionListener {
 		this.personEdited = person;
 		this.home = home;
 		
+		try {
+			storage = DataStorage.getMainDataStorage();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		editing = false;
 
-		name = new JTextField(10);
-		cultureChoices = new Vector<String>(Arrays.asList("None", "French", "American", "Italian", "German", "Other"));
-		culture = new JComboBox<>(new String[] { "None", "French", "American", "Italian", "German", "Other" });
-		genderChoices = new Vector<String>(Arrays.asList("", "Male", "Female", "Other"));
-		gender = new JComboBox<>(new String[] { "None", "Male", "Female", "Other" });
-		occupationChoices = new Vector<String>(Arrays.asList("", "Artist", "Bartender", "Other"));
-		occupation = new JComboBox<>(new String[] { "None", "Artist", "Bartender", "Other" });
-		notes = new JTextArea(2, 10);
+		name = new JTextField(15);
+		nodeName = new JTextField(15);
+		cultureChoices = storage.getCultureChoices();
+		culture = new JComboBox<>(cultureChoices.toArray());
+		genderChoices = new Vector<String>(Arrays.asList("Unknown", "Male", "Female"));
+		gender = new JComboBox<>(new String[] { "Unknown", "Male", "Female"});
+		occupationChoices = storage.getOccupationChoices();
+		occupation = new JComboBox<>(occupationChoices.toArray());
+		notes = new JTextArea(2, 15);
 		notes.setLineWrap(true);
 		submitButton = new JButton("Submit");
 		cancel = new JButton("Cancel");
 		deleteButton = new JButton("Delete");
-		addData = new JButton("Add Data");
-
-		nameLabel = new JLabel("Artist Name:");
+		
+		nameLabel = new JLabel("Person Name:");
+		nodeNameLabel = new JLabel("Node Name:");
 		culturalLabel = new JLabel("Cultural ID:");
 		genderLabel = new JLabel("Gender:");
 		occupationLabel = new JLabel("Occupation:");
 		biographyLabel = new JLabel("Biography:");
 
 		namePanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
+		nodeNamePanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
 		culturePanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
 		genderPanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
 		occupationPanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
@@ -101,6 +112,7 @@ public class AddEditPersonGUI implements ActionListener {
 		scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 
 		namePanel.add(name);
+		nodeNamePanel.add(nodeName);
 		culturePanel.add(culture);
 		genderPanel.add(gender);
 		occupationPanel.add(occupation);
@@ -118,7 +130,6 @@ public class AddEditPersonGUI implements ActionListener {
 		submitButton.addActionListener(this);
 		cancel.addActionListener(this);
 		deleteButton.addActionListener(this);
-		addData.addActionListener(this);
 
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -139,8 +150,9 @@ public class AddEditPersonGUI implements ActionListener {
 	 */
 
 	private JPanel createCenterPanel() {
-		centerPanel = new JPanel(new GridLayout(5, 1));
+		centerPanel = new JPanel(new GridLayout(6, 1));
 		centerPanel.add(namePanel);
+		centerPanel.add(nodeNamePanel);
 		centerPanel.add(culturePanel);
 		centerPanel.add(genderPanel);
 		centerPanel.add(occupationPanel);
@@ -155,8 +167,9 @@ public class AddEditPersonGUI implements ActionListener {
 	 * @return JPanel This returns the completed west panel.
 	 */
 	private JPanel createWestPanel() {
-		westPanel = new JPanel(new GridLayout(5, 1));
+		westPanel = new JPanel(new GridLayout(6, 1));
 		westPanel.add(nameLabel);
+		westPanel.add(nodeNameLabel);
 		westPanel.add(culturalLabel);
 		westPanel.add(genderLabel);
 		westPanel.add(occupationLabel);
@@ -176,8 +189,6 @@ public class AddEditPersonGUI implements ActionListener {
 		southPanel.add(cancel);
 		if(editing){
 			southPanel.add(deleteButton);
-		} else {
-			southPanel.add(addData);
 		}
 		return southPanel;
 	}
@@ -194,6 +205,7 @@ public class AddEditPersonGUI implements ActionListener {
 	 */
 	void setDefault() {
 		name.setText("");
+		nodeName.setText("");
 		culture.setSelectedIndex(0);
 		gender.setSelectedIndex(0);
 		occupation.setSelectedIndex(0);
@@ -211,6 +223,7 @@ public class AddEditPersonGUI implements ActionListener {
 	void setPersonData(Person person) {
 		Person personToEdit = person;
 		name.setText(personToEdit.getName());
+		nodeName.setText(personToEdit.getNodeName());
 		culture.setSelectedIndex(cultureChoices.indexOf(personToEdit.getCulturalId()));
 		gender.setSelectedIndex(genderChoices.indexOf(personToEdit.getGender()));
 		occupation.setSelectedIndex(occupationChoices.indexOf(personToEdit.getOccupation()));
@@ -252,9 +265,9 @@ public class AddEditPersonGUI implements ActionListener {
 	 * @throws IOException
 	 */
 	private void submitClicked() throws IOException {
-		DataStorage storage = DataStorage.getMainDataStorage();
 		if (personEdited != null) {
 			personEdited.setName(name.getText());
+			personEdited.setNodeName(nodeName.getText());
 			personEdited.setOccupation(occupation.getSelectedItem().toString());
 			personEdited.setGender(gender.getSelectedItem().toString());
 			personEdited.setCulturalId(culture.getSelectedItem().toString());
@@ -262,7 +275,7 @@ public class AddEditPersonGUI implements ActionListener {
 		} else {
 			int nextID = storage.incrementAndGetNextPersonIdNum();
 
-			Person newPerson = new Person(nextID, name.getText(), occupation.getSelectedItem().toString(),
+			Person newPerson = new Person(nextID, name.getText(), nodeName.getText(), occupation.getSelectedItem().toString(),
 					gender.getSelectedItem().toString(), culture.getSelectedItem().toString(), notes.getText());
 
 			storage.addPerson(newPerson);
@@ -292,8 +305,6 @@ public class AddEditPersonGUI implements ActionListener {
 			frame.dispose();
 		} else if (event.getSource() == deleteButton){
 			//delete this person...why tho?
-		} else if (event.getSource() == addData){
-			AddData add = new AddData(FIELDS);
 		}
 	}
 }
